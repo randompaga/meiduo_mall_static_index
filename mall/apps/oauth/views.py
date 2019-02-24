4d2b2b641a826136e7db5f8d3c651ccd2aa1dc9f
itcast <qiruihua@itcast.cn>
2019-02-24 15:31:18 +0800

    12-获取openid的思路分析

--- mall/apps/oauth/views.py
+++ mall/apps/oauth/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+"""
+对于应用而言，需要进行两步：
+1. 获取Authorization Code；        其实就是通过 url来得到用户的同意
+
+2. 通过Authorization Code获取Access Token
+
+
+对于我们来说,
+3.通过token 换取openid
+
+"""
 
 """
 https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101474184&redirect_uri=http://www.meiduo.site:8080/oauth_callback.html&state=test
@@ -24,6 +35,7 @@ https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=101474184&r
 
 五.编码
 """
+
 from QQLoginTool.QQtool import OAuthQQ
 from mall import settings
 class OauthQQURLAPIView(APIView):
@@ -43,3 +55,33 @@ class OauthQQURLAPIView(APIView):
 
         return Response({"auth_url":auth_url})
 
+"""
+一,需求:
+    当用户点击同意之后,qq认证服务器会返回一个code,这个时候,需要让前端将
+    code返回给后端
+
+二.思路(步骤)
+
+    1.接收code
+    2.通过code换取token
+    3.通过token换取openid
+    4.
+三. 请求方式和路由
+    GET     /oauth/qq/users/?code=xxxx
+
+    POST    /oauth/qq/users/        body code:xxxx
+四. 确定选择哪个视图
+    APIView:
+    GenericAPIView:
+    ListAPIVIew,RetrieveAPIView:
+
+"""
+
+class QQOauthUserAPIView(APIView):
+
+    def get(self,request):
+        # 1.接收code
+        # 2.通过code换取token
+        # 3.通过token换取openid
+
+        pass
